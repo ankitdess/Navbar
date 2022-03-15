0b86997017a3c0bb989b09c4fe82ea495e8441dd
ankitdess <sendtoankitzz@gmail.com>
2022-03-15 10:08:19 +0530

    hello

---
+++ app.js
@@ -0,0 +1,13 @@
+var text = ["Loading your content..", "This Shouldn't take much time", "Hang on few sec.."];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 3000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
